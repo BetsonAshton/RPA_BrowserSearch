ec678a41995c39f7996388665a55fcf610c6d72a
BetsonAshton <luisrobertoashton@gmail.com>
2024-02-07 12:49:58 -0300

    fisrt commit

---
+++ RPA Browser Search/Program.cs
@@ -0,0 +1,11 @@
+﻿using EasyAutomationFramework;
+using OpenQA.Selenium;
+
+var web = new Web();
+
+web.StartBrowser();
+
+web.Navigate("https://google.com");
+
+web.AssignValue(TypeElement.Name, "q", "Hello World!").element.SendKeys(Keys.Enter);
+
